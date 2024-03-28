0fb88a600f111faa99c1a5b908003f3f590e7b01
Dimas Adam Saputra <dimasadam058@gmail.com>
2024-03-28 23:16:36 +0700

    Initial commit

---
+++ config/test.exs
@@ -0,0 +1,33 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :hello_phoenix, HelloPhoenix.Repo,
+  username: "root",
+  password: "",
+  hostname: "localhost",
+  database: "hello_phoenix_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello_phoenix, HelloPhoenixWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "JTEyHucPD94bkqCm5EL5pi6p3Ewa21/ta65OSFqNoauZjNs7x1BpBMUzGOrufCj+",
+  server: false
+
+# In test we don't send emails.
+config :hello_phoenix, HelloPhoenix.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
